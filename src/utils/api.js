7284af616c815c7190d08d91201209d7eeebb14f
Haeven Kelley <haevenshane@gmail.com>
2024-02-16 15:05:06 -0600

    Initial commit - repentance, worship, warfare

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key": 'fc13113b51mshee0c1ad32f50918p14c29bjsn95db2f706d46',
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
